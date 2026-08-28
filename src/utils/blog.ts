89a9615497b14e1a6d95c1cfab3efef9a99d4d0c
jviglab <joavig@icloud.com>
2026-08-28 16:15:04 -0400

    fix: agregar getStaticPathsBlogPost y blogPostRobots a blog.ts

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -10,3 +10,15 @@ export const getBlogEntryBySlug = async (slug: string) => {
   const posts = await getCollection('post');
   return posts.find(post => post.slug === slug);
 };
+
+export const getStaticPathsBlogPost = async () => {
+  const posts = await getBlogEntries();
+  return posts.map((post) => ({
+    params: { slug: post.slug },
+    props: { post },
+  }));
+};
+
+export const blogPostRobots = () => {
+  return 'index, follow';
+};
